d532baa51d5557bce1182f7dac9fc5207303fdf7
bryant.howell <bryant.howell@thoughtspot.com>
2022-08-15 14:18:19 -0500

    Addded TSTypes ENUM to combine Metadata Type and Sub-Type for ease of programming

--- src/thoughtspot_rest_api_v1/__init__.py
+++ src/thoughtspot_rest_api_v1/__init__.py
@@ -1,6 +1,6 @@
 from .tsrestapiv1 import TSRestApiV1
 from .tsrestapiv1 import (
-    MetadataNames, MetadataSorts, MetadataSubtypes, MetadataCategories,
+    MetadataNames, MetadataSorts, TSTypes, MetadataSubtypes, MetadataCategories,
     ShareModes, Privileges, PermissionTypes
 )
 from ._version import __version__
