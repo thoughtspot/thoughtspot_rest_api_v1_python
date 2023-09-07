70f599370aa1ce24836209d32371c91d8a6b9c41
bryant.howell <bryant.howell@thoughtspot.com>
2023-09-07 14:12:14 -0500

    Removed the metadata_details lookup out of the action loop, so if there are multiple actions to associate to, there is only one call to metadata_details now

--- examples/custom_actions.py
+++ examples/custom_actions.py
@@ -45,61 +45,61 @@ def find_action_guid(display_name_to_find: str = None, action_js_id_to_find: str
 # If you know set of action GUIDs to associate to, provide list here to skip lookups
 action_guids = []
 
+
+# The action_associations_post endpoint allows for APPENDS, so you do not need any of this information
+# Simply provided as example of calls if you do want to get the specific details
+# action_info = ts.admin_embed_action(action_guid=action_guid)
+# print(json.dumps(action_info, indent=2))
+
+# Existing association info will appear under 'actionAssociationMap' key
+# print(json.dumps(action_info['actionAssociationMap'], indent=2))
+
+# To associate all Vizes on a Liveboard, use either /metadata/listvizheaders or /metadata/tml/export
+lb_guid = 'c7366d05-dc19-4aae-8f72-e8acf1d641e1'
+viz_headers = ts.metadata_listvizheaders(guid=lb_guid)
+
+# TML can be parsed to give you info like chart.type, to skip KPI charts or otherwise with assignment
+# Or to find any formulas defined at the viz level, which might also be reason to skip
+lb_tml_obj = ts.metadata_tml_export(guid=lb_guid, export_fqn=True)
+
+viz_guids_to_add_to_action =[]
+for v in lb_tml_obj['liveboard']['visualizations']:
+    viz_guid = v['viz_guid']
+    print(viz_guid)
+    # Not every viz has 'answer' key, could be 'note_tile' now
+    if 'answer' in v:
+        chart_type = v['answer']['chart']['type']
+        print(chart_type)
+        if chart_type not in ['KPI']:
+            # Skip any with a formula defined
+            if 'formulas' not in v['answer']:
+                viz_guids_to_add_to_action.append(viz_guid)
+
+
+print("Viz GUIDs to add to action: ")
+print(viz_guids_to_add_to_action)
+
+# The Custom Actions APIs use another GUID, different from the viz_guids seen in TML
+# You must call /metadata/details and find the refAnswerBook.id
+# ['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"]
+# ['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"][0]["vizContent"]["refAnswerBook"]["id"]
+# where the ['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"][0]['header']['id']
+# matches to the viz_id
+
+# Container for the internal 'refAnswerBook' viz ids used by the Actions
+internal_viz_guids_to_add_to_action = []
+
+details = ts.metadata_details(object_type=TSTypes.LIVEBOARD, object_guids=[lb_guid])
+
+# /metadata/details is very complex structure, this gets to the part with info on the visualizations
+vizes = details['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"]
+for v in vizes:
+    internal_id = v["vizContent"]["refAnswerBook"]["id"]
+    external_viz_guid = v['header']['id']
+    if external_viz_guid in viz_guids_to_add_to_action:
+        internal_viz_guids_to_add_to_action.append(internal_id)
+
 for action_guid in action_guids:
-
-    # The action_associations_post endpoint allows for APPENDS, so you do not need any of this information
-    # Simply provided as example of calls if you do want to get the specific details
-    # action_info = ts.admin_embed_action(action_guid=action_guid)
-    # print(json.dumps(action_info, indent=2))
-
-    # Existing association info will appear under 'actionAssociationMap' key
-    # print(json.dumps(action_info['actionAssociationMap'], indent=2))
-
-    # To associate all Vizes on a Liveboard, use either /metadata/listvizheaders or /metadata/tml/export
-    lb_guid = 'c7366d05-dc19-4aae-8f72-e8acf1d641e1'
-    viz_headers = ts.metadata_listvizheaders(guid=lb_guid)
-
-    # TML can be parsed to give you info like chart.type, to skip KPI charts or otherwise with assignment
-    # Or to find any formulas defined at the viz level, which might also be reason to skip
-    lb_tml_obj = ts.metadata_tml_export(guid=lb_guid, export_fqn=True)
-
-    viz_guids_to_add_to_action =[]
-    for v in lb_tml_obj['liveboard']['visualizations']:
-        viz_guid = v['viz_guid']
-        print(viz_guid)
-        # Not every viz has 'answer' key, could be 'note_tile' now
-        if 'answer' in v:
-            chart_type = v['answer']['chart']['type']
-            print(chart_type)
-            if chart_type not in ['KPI']:
-                # Skip any with a formula defined
-                if 'formulas' not in v['answer']:
-                    viz_guids_to_add_to_action.append(viz_guid)
-
-
-    print("Viz GUIDs to add to action: ")
-    print(viz_guids_to_add_to_action)
-
-    # The Custom Actions APIs use another GUID, different from the viz_guids seen in TML
-    # You must call /metadata/details and find the refAnswerBook.id
-    # ['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"]
-    # ['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"][0]["vizContent"]["refAnswerBook"]["id"]
-    # where the ['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"][0]['header']['id']
-    # matches to the viz_id
-
-    # Container for the internal 'refAnswerBook' viz ids used by the Actions
-    internal_viz_guids_to_add_to_action = []
-
-    details = ts.metadata_details(object_type=TSTypes.LIVEBOARD, object_guids=[lb_guid])
-
-    # /metadata/details is very complex structure, this gets to the part with info on the visualizations
-    vizes = details['storables'][0]["reportContent"]["sheets"][0]["sheetContent"]["visualizations"]
-    for v in vizes:
-        internal_id = v["vizContent"]["refAnswerBook"]["id"]
-        external_viz_guid = v['header']['id']
-        if external_viz_guid in viz_guids_to_add_to_action:
-            internal_viz_guids_to_add_to_action.append(internal_id)
-
     #
     # Format for actionAssociationMap is something like:
     #
