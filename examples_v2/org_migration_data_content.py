1f28f6a03b678a5ec886ed8fa4e12578dd59d8ec
bryant.howell <bryant.howell@thoughtspot.com>
2024-05-21 13:25:09 -0500

    Added start of org_migration_data_content.py

--- examples_v2/org_migration_data_content.py
+++ examples_v2/org_migration_data_content.py
@@ -61,34 +61,15 @@ except requests.exceptions.HTTPError as e:
     exit()
 
 
-def create_new_connection_from_primary(connection_name_to_replicate, new_connection_name, new_connection_password):
-    # Fill in more details if you need more specific
-    # "include_details: True lets us get the connection_config to use in the creation step
-    connection_id_req = {
-        "record_offset": 0,
-        "record_size": 10,
-        "authentication_type": "SERVICE_ACCOUNT",
-        "include_details": True,
-        "connections": [
-            {
-                "identifier": connection_name_to_replicate
-            }
-        ]
-    }
-    conn_search_resp = org0.connection_search()
-    if len(conn_search_resp) == 1:
-        connection_id_to_replicate = conn_search_resp[0]["id"]
-    else:
-        print("Connection name {} returns more than one connection, specify ID".format(connection_name_to_replicate))
-        exit()
+def create_new_connection_from_primary(connection_to_replicate, new_connection_name, new_connection_password):
 
     # Create connection on destination org (need to get credentials from secure storage)
-    orig_conn_config = conn_search_resp[0]["details"]["configuration"]
+    orig_conn_config = connection_to_replicate["details"]["configuration"]
     orig_conn_config["password"] = new_connection_password
     # Must replace the password, it w
     conn_create_req = {
           "name": new_connection_name,
-          "data_warehouse_type": conn_search_resp[0]["data_warehouse_type"],
+          "data_warehouse_type": connection_to_replicate["data_warehouse_type"],
           "data_warehouse_config": orig_conn_config,
           "validate": False
     }
@@ -105,17 +86,46 @@ def create_new_connection_from_primary(connection_name_to_replicate, new_connect
 def main():
     connection_name_to_replicate = "Customer A Connection"
     new_connection_name = "Main Connection"  # change if you need to, you can have same name across Orgs if you like
+    # Fill in more details if you need more specific
+    # "include_details: True lets us get the connection_config to use in the creation step
+    connection_id_req = {
+        "record_offset": 0,
+        "record_size": 10,
+        "authentication_type": "SERVICE_ACCOUNT",
+        "include_details": True,
+        "connections": [
+            {
+                "identifier": connection_name_to_replicate
+            }
+        ]
+    }
+    conn_search_resp = org0.connection_search()
+    if len(conn_search_resp) == 1:
+        conn_to_replicate = conn_search_resp[0]
 
+    else:
+        print("Connection name {} returns more than one connection, specify ID".format(connection_name_to_replicate))
+        exit()
+
+    connection_id_to_replicate = conn_search_resp[0]["id"]
     # Or alternatively, create the connection in the UI
-    new_conn_id = create_new_connection_from_primary(connection_name_to_replicate=connection_name_to_replicate,
+    new_conn_id = create_new_connection_from_primary(connection_to_replicate=conn_to_replicate,
                                                      new_connection_name=new_connection_name,
                                                      new_connection_password="{getCredentialsSomehow}")
 
     # new_conn_id = "{guidCopiedFromUI}"
 
     # Get all data objects from the primary org on particular connection
+    # Tables come from /connection/search -> ["details"]["tables"]
+    tables_on_orig_conn = conn_to_replicate["details"]["tables"]
 
+    # From the tables, you can use /metadata/search to retrieve all dependents,
+    # or use /metadata/tml/export with "export_associated": true
 
+    # For the sake of reducing duplication, make first calls to get the total set of TML to export / reimport
+
+    # If Connection Name is different between the two orgs, you'll need to adjust the TML objects
+    # use https://github.com/thoughtspot/thoughtspot_tml library for each object access all properties
 
 if __name__ == "__main__":
     main()
